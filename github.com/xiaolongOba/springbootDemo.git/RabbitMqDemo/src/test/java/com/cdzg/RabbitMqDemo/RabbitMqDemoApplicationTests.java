66b51884ce837b1c6cb41528d61b5daf86d4818b
wangxiaolong <wangxiaolong@cdzg.com>
2019-06-25 09:25:00 +0800

    一对一的队列demo

--- github.com/xiaolongOba/springbootDemo.git/RabbitMqDemo/src/test/java/com/cdzg/RabbitMqDemo/RabbitMqDemoApplicationTests.java
+++ github.com/xiaolongOba/springbootDemo.git/RabbitMqDemo/src/test/java/com/cdzg/RabbitMqDemo/RabbitMqDemoApplicationTests.java
@@ -2,15 +2,27 @@ package com.cdzg.RabbitMqDemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cdzg.RabbitMqDemo.sender.HelloSender;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RabbitMqDemoApplicationTests {
+	@Autowired
+	private HelloSender helloSender;
 
 	@Test
-	public void contextLoads() {
+	public void sayHello() {
+		helloSender.send();
+	}
+
+	@Test
+	public void sayIndex() {
+		helloSender.send();
+
 	}
 
 }
